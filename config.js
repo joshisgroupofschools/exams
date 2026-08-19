688d02234f5475920ae2a90de2549ec6fda664d1
joshisgroupofschools <joshisvidyaniketan@gmail.com>
2026-08-19 23:54:43 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -2,4 +2,3 @@ window.APP_CONFIG = window.APP_CONFIG || {
   appName: 'Exam Management Software',
   apiBaseUrl: 'https://script.google.com/macros/s/AKfycbyCFTEoCSfGtkYn2PkXe0wEjszh8OFur5Jc-ooOD52H0xhaOaDlIsC_SWOnFx5mUeafyw/exec'
 };
-
